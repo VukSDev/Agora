a117af111cd36adf03352fa75f55df448c8dad09
VukSDev <vukspalajkovic@gmail.com>
2019-10-26 17:27:16 +0200

    rotate on coin

---
+++ Agora/Assets/Scripts/Rotate.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public float speed = 90;
+    void Update()
+    {
+        transform.RotateAround(transform.position, Vector3.up, speed * Time.deltaTime);
+    }
+}
